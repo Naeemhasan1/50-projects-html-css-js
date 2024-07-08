f8d9c2d4d1373a7d04d05dd41f1cecfc7ca4393f
Naeemhasan1 <csnaeem4b@gmail.com>
2024-07-09 03:30:21 +0600

    Create and complete expanding cards project

---
+++ 0.Expanding Cards/script.js
@@ -0,0 +1,14 @@
+// alert("it works!!");
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClass();
+    panel.classList.add("active");
+  });
+});
+function removeActiveClass() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
